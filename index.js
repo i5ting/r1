d6aff6a25fb662892b957036283f805bb7d530a1
i5ting <i5ting@126.com>
2016-10-20 22:43:36 +0800

    lazycommmit, please ignore

--- index.js
+++ index.js
@@ -3,5 +3,25 @@
 var argv = process.argv;
 argv.shift();
 
-var file_path = __dirname;
-var current_path = process.cwd();
+console.log('r1')
+
+
+var is_open = true;
+var markd_config = {
+	debug: false
+}
+
+//函数可以返回当前正在执行的项目路径
+var pwd = process.cwd()  
+
+var source_file_name = pwd + '/README.md'
+var file_name = source_file_name.split('/').pop();;
+var _file_name = file_name.split('.')[0];
+
+var dest_file_path = pwd + '/preview/' + _file_name + '.html';
+
+console.log('pwd=' + pwd);
+console.log('source_file_name=' + source_file_name);
+console.log('dest_file_path=' + dest_file_path);
+
+require('i5ting_toc')(pwd, source_file_name, dest_file_path, is_open, markd_config);
